e024c228b3659010e6324d7c0c9045dab34f3687
bensultan1985 <bensultan1985@gmail.com>
2020-01-05 11:54:46 -0800

    changed for testing purposes

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
-let globe = document.getElementById('globe');
+let globe1 = document.getElementById('globe');
 
 let ranNum = 0
+
+function startRound () {
+
+    globe1.innerHTML = 'Good luck!';
+
+  }
 const randomNumber = () => {
     ranNum = Math.floor(Math.random()*4)
 };
 
-globe.onclick = () => {
-    globe.innerHTML = 'hello';
+globe1.onclick = () => {
+    globe1.innerHTML = 'hello';
 };
 
 ranNum();
+startRound();
+
+globe1.innerHTML = 'hello'
+
+let button = document.getElementById('test').innerHTML = 'changes'
