4ee76dc5ce87d43f4b3e797deeb3d4721e5f9b78
bensultan1985 <bensultan1985@gmail.com>
2020-01-09 13:52:50 -0800

    working javascript

--- javascript.js
+++ javascript.js
@@ -1,23 +1,7 @@
-let globe1 = document.getElementById('globe');
+alert("sometext");
 
-let ranNum = 0
+globe = document.getElementById('globe');
+globe.innerHTML = 'hi';
 
-function startRound () {
-
-    globe1.innerHTML = 'Good luck!';
-
-  }
-const randomNumber = () => {
-    ranNum = Math.floor(Math.random()*4)
-};
-
-globe1.onclick = () => {
-    globe1.innerHTML = 'hello';
-};
-
-ranNum();
-startRound();
-
-globe1.innerHTML = 'hello'
-
-let button = document.getElementById('test').innerHTML = 'changes'
+alert("moretext");
+alert("eventext");
