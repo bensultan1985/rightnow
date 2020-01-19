82ef13450a2b755b2299b9776726b1a9242325ce
bensultan1985 <bensultan1985@gmail.com>
2020-01-19 15:02:30 -0800

    organized code

--- javascript.js
+++ javascript.js
@@ -16,3 +16,20 @@ const initMap = () => {
 //globe = document.getElementById('map');
 //globe.innerHTML = 'hi';
 
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '199712351201296',
+      xfbml      : true,
+      version    : 'v5.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
